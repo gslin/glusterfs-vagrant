01a80b021f0265e9eb1ecb36c6da52902a12192f
Gea-Suan Lin <gslin@gslin.org>
2016-05-26 20:07:26 +0800

    Remove peer probe.

--- gfs.sh
+++ gfs.sh
@@ -9,10 +9,6 @@ work()
     echo "/dev/sdb /mnt ext4 defaults 1 2" >> /etc/fstab
     mount -a
     mkdir /mnt/gv0
-
-    if [[ "x$(hostname)" != "xgfs1" ]]; then
-        gluster peer probe 192.168.50.101
-    fi
 }
 
 work > /tmp/boot.log 2>&1 &
