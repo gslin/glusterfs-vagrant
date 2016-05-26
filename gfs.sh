27bf23cf07a231a4d9683749472b4ed7711b4c53
Gea-Suan Lin <gslin@gslin.org>
2016-05-27 04:14:40 +0800

    Change to use loop, and drop extra disk requirement.

--- gfs.sh
+++ gfs.sh
@@ -4,10 +4,6 @@ work()
 {
     apt-get -Vy update
     apt-get -Vy install glusterfs-server
-
-    echo y | mkfs.ext4 /dev/sdb
-    echo "/dev/sdb /mnt ext4 defaults 1 2" >> /etc/fstab
-    mount -a
     mkdir /mnt/gv0
 }
 
